ab165b4d72235806b181621bf6a9505aa8c33bd7
hugogdiniz <hugogdiniz.ti@gmail.com>
2022-10-11 11:26:42 -0300

    inicializando projeto

---
+++ src/main/java/br/me/desafio/fullstackchallenger/FullstackChallengerApplication.java
@@ -0,0 +1,13 @@
+package br.me.desafio.fullstackchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackChallengerApplication.class, args);
+	}
+
+}
