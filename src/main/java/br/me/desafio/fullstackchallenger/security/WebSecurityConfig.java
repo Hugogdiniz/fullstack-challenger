29e27cd6808d3cb99fae580302d6764cd102e0cf
hugogdiniz <hugogdiniz.ti@gmail.com>
2022-10-18 00:24:43 -0300

    inicio de implementação do spring security

---
+++ src/main/java/br/me/desafio/fullstackchallenger/security/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package br.me.desafio.fullstackchallenger.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+// Mapeia URL, endereços, autoriza ou bloqueia acessos a URL
+@Configuration
+public class WebSecurityConfig {
+
+}
